220e1b677fc52ba02d305bb10add470743ff5126
bjh <brodie.hanbuch@gmail.com>
2013-03-21 09:33:12 -0700

    launch sequence completed, preparing for take off

--- lib/bruce-banner.rb
+++ lib/bruce-banner.rb
@@ -1,5 +1,23 @@
 require "bruce-banner/version"
 
 module BruceBanner
+  module ::Kernel
+    def bb(what="", options={})
+      options = {
+        count:40,
+        string:'-',
+        before:true,
+        after:true,
+        size_to_fit:true
+      }.merge(options)
 
+      if options[:size_to_fit]
+        options[:count] = what.to_s.size
+      end
+
+      puts(options[:string] * options[:count]) if options[:before]
+      puts "#{what}"
+      puts(options[:string] * options[:count]) if options[:after]
+    end
+  end
 end
